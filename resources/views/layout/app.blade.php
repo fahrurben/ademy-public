aa0d8b1a165b9c0b7dce67618648409d629d48ba
Fahrur Razi <fahrurben@gmail.com>
2020-05-14 05:45:27 +0700

    Fix view modal header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -126,10 +126,8 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">View</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">View</h4>
             </div>
             <div id="modalViewBody" class="modal-body">
             </div>
